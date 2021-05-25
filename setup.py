6fffda299d715a98b0cd79887366178e3d470f40
dineshsonachalam <dineshsonachalam@gmail.com>
2021-05-25 18:18:16 +0530

    Code cleanup

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
 from setuptools import find_packages, setup
+import requests
+import semantic_version
+
+def get_LucidDynamodb_version():
+    url = "https://pypi.org/pypi/LucidDynamodb/json"
+    response = requests.request("GET", url, headers={}, data={})
+    result = response.json()
+    LucidDynamodb_version = str(result.get("info").get("version"))
+    current_version = semantic_version.Version(LucidDynamodb_version)
+    next_version = current_version.next_patch()
+    return next_version
 
 with open('requirements.txt', 'rt') as requirements:
     requires = requirements.readlines()
     setup(
             name="LucidDynamodb",
-            version="1.0.1",
+            version=str(get_LucidDynamodb_version()),
             author="Dinesh Sonachalam",
             author_email="dineshsonachalam@gmail.com",
             description="A simple Python wrapper to AWS Dynamodb",
